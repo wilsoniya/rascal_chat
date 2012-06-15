2605b0260714b1a9e7597c2be8da391b2e2e5265
Michael Wilson <wilsoniya@gmail.com>
2012-06-15 15:13:01 -0500

    initial commit

---
+++ client/python/client.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import sys
+import socket
+import time
+
+from gevent import monkey; monkey.patch_all()
+import gevent
+from gevent.socket import wait_read
+
+PORT = 20000
+IP = '127.0.0.1'
+IP2 = '67.175.163.247'
+IP3 = '192.168.1.110'
+HOST = 'wayne.wallitt.com'
+MESSAGE = 'print;Fart'
+BUFSIZE = 2**10
+
+OP_REGISTER = 'register'
+OP_SEND = 'send'
+
+if len(sys.argv) > 1:
+    message = sys.argv[1]
+else:
+    message = 'ADFASDFASDF'
+
+def socket_reader(sock):
+    while True:
+        recvd = sock.recv(BUFSIZE)
+        sender, message = recvd.split(';')
+        print '{}: {}'.format(sender, message)
+
+def client(sock):
+    gevent.spawn(socket_reader, sock)
+    while True:
+        sys.stdout.write('message: ')
+        sys.stdout.flush()
+        wait_read(sys.stdin.fileno())
+        outbound_msg = sys.stdin.readline()
+        send_command = ';'.join((OP_SEND, outbound_msg))
+        sock.send(send_command)
+
+def startup():
+    while True:
+        username = raw_input('Your username: ')
+        if len(username.strip()) > 0:
+            break
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+    register_command= ';'.join((OP_REGISTER, username))
+    s.send(register_command)
+
+    register_response = s.recv(BUFSIZE)
+    if register_response == "ok":
+        print 'connected'
+        client(s)
+    elif register_response == "username_exists":
+        print 'username in use. exiting.'
+        s.close() 
+
+def main():
+    g = gevent.spawn(startup)
+    g.join()
+
+if __name__ == '__main__':
+    main()
---
+++ server/server.erl
@@ -0,0 +1,99 @@
+#!/usr/bin/env escript
+
+start_server() ->
+    Opts = [{packet, 0}, {reuseaddr, true}, {active, true}, {ip, {192,168,1,110}}],
+    {ok, Listen} = gen_tcp:listen(20000, Opts),
+    init_ets(),
+    spawn(fun() -> connection_server(Listen) end),
+    sleep(infinity).
+
+init_ets() ->
+    ets:new(user_socks, [set, named_table, public]).
+
+sleep(Duration) ->
+    receive
+    after Duration ->
+        true
+    end.
+
+connection_server(Listen) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    spawn(fun() -> connection_server(Listen) end),
+    inet:setopts(Socket, [{packet, 0}, {nodelay, true}, {active, true}]),
+    client(Socket).
+
+client(Socket) ->
+    receive
+        {tcp, Socket, Msg} ->
+            [Op|Args] = string:tokens(Msg, ";"),
+            case Op of 
+                "print" ->
+                    io:format("~p~n", Args),
+                    client(Socket);
+                "register" ->
+                    [Username|_] = Args,
+                    case register_user(Username) of 
+                        username_exists ->
+                            io:format("Username exists: ~s~n", [Username]),
+                            gen_tcp:send(Socket, "username_exists");
+                        ok ->
+                            put(username, Username),
+                            io:format("Username registered: ~s~n", [Username]),
+                            gen_tcp:send(Socket, "ok")
+                    end,
+                    client(Socket);
+                "deregister" ->
+                    deregister_user(get(username)),
+                    client(Socket);
+                "send" ->
+                    [Message|_] = Args,
+                    io:format("got message: ~s~n", [Message]),
+                    Username = get(username),
+                    spawn(fun() -> send_message(Message, Username) end),
+                    client(Socket);
+                Other ->
+                    io:format("Unknown op; exiting~n"),
+                    io:format("op: ~p~n", [Op]),
+                    io:format("message body: ~p~n", [Msg])
+            end;
+        {tcp_closed, Socket} ->
+            Username = get(username),
+            erase(username),
+            ets:delete(user_socks, Username),
+            io:format("~s has gone offline~n", [Username]);
+        {inbound_msg, Msg, Sender} ->
+            gen_tcp:send(Socket, string:join([Sender, Msg], ";")),
+            client(Socket)
+    end.
+
+deregister_user(Username) ->
+    ets:delete(user_socks, Username).
+
+register_user(Username) ->
+    ClientPid = self(),
+    ExistingUsernames = ets:lookup(user_socks, Username),
+    if
+        length(ExistingUsernames) > 0 ->
+            username_exists;
+        true ->
+            ets:insert(user_socks, {Username, ClientPid}),
+            ok
+    end.  
+
+send_message(Msg, Sender) ->
+    Send = fun({Username, ClientPid}, _) ->
+        if 
+            Sender /= Username ->
+                ClientPid ! {inbound_msg, Msg, Sender},
+                ok;
+            true ->
+                ok
+        end,
+        none
+    end,
+    ets:foldl(Send, none, user_socks),  
+    ok.
+
+main(_) ->
+    start_server().
+
